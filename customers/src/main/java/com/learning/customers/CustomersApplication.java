77546e46b06b09b9a4955d200089c2b68b674d8b
NadaElazab <mksalama@Alinma.com>
2024-07-23 23:37:50 +0300

    add customer to eureka server

--- customers/src/main/java/com/learning/customers/CustomersApplication.java
+++ customers/src/main/java/com/learning/customers/CustomersApplication.java
@@ -2,12 +2,10 @@ package com.learning.customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("api/v1")
+@EnableEurekaClient
 public class CustomersApplication {
 
     public static void main(String[] args) {
